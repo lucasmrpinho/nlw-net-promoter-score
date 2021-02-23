ffb945feaef7a336a416a479959e71a0ad9dca33
lucasmrpinho <lucaspinho101@gmail.com>
2021-02-23 13:40:13 -0300

    Database configured

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+import express from 'express';
+import "./database";
+import { router } from "./routes";
+
+const app = express();
+
+app.use(router);
+
+app.listen(3333, () => console.log("Server is runnig!"));
