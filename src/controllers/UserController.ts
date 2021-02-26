6aa0dae981aebd1675df5869de38c2a4fb771b3b
lucasmrpinho <lucaspinho101@gmail.com>
2021-02-26 12:50:56 -0300

    nps complete

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
-import { UsersRepository } from '../repositories/UserRepository';
+import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup';
 
 class UserController {
     async create(request: Request, response: Response){
         const { name, email } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            enail: yup.string().email().required()
+        })
         
+        if(!(await !schema.isValid(request.body))) {
+            return response.status(400).json({error: "Validation failed!"});
+        }
+
         const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
@@ -25,7 +35,7 @@ class UserController {
 
         await usersRepository.save(user);
 
-        return response.json(user);
+        return response.status(201).json(user);
     }
 }
 
